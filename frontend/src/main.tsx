bcfefb6368dd657c49e3e78e2b34abf91176f39c
1sthijju <129365466+1sthijju@users.noreply.github.com>
2026-09-18 17:43:12 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'movi-player'; // registers the <movi-player> custom element
+import 'movi-player';
 import App from './App';
 import './index.css';
 
