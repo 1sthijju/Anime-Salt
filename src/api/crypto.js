cf636ca3700bec0cabd8c097aa88657018ac2c89
1sthijju <129365466+1sthijju@users.noreply.github.com>
2026-09-19 22:23:57 +0530

    Delete src/api directory

--- src/api/crypto.js
+++
@@ -1,8 +0,0 @@
-export async function decryptAES(ciphertext, key, iv, mode = "CBC") {
-  const algo = { name: `AES-${mode}`, iv: new Uint8Array(iv) };
-  const cryptoKey = await crypto.subtle.importKey(
-    "raw", new Uint8Array(key), algo, false, ["decrypt"]
-  );
-  const decrypted = await crypto.subtle.decrypt(algo, cryptoKey, new Uint8Array(ciphertext));
-  return new TextDecoder().decode(decrypted);
-}
